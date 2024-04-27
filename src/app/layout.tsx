eed552e0e3b4b7036a388db26b81acbab625685b
Sean Collins <sean.collins@outlook.com>
2024-04-27 00:17:46 -0700

    Migrate common page styling to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          {children}
+          <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+            {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+    <>
       <h1>Hello, world!</h1>
-    </main>
+    </>
   );
 }
