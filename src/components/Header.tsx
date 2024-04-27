29a20a011df8bd9608ef6bde0e3e14a18999f18a
Sean Collins <sean.collins@outlook.com>
2024-04-27 00:13:09 -0700

    Fix component export typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Menu, Package2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
-export function Header() {
+export default function Header() {
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
@@ -96,5 +96,5 @@ export function Header() {
         </SheetContent>
       </Sheet>
     </header>
-  )
-}
+  );
+};
