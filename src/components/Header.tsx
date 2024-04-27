bccc8284d6b997ffc6dc5b5a919fc3cb7139c739
Sean Collins <sean.collins@outlook.com>
2024-04-27 00:21:04 -0700

    Update navigation links to match page plugs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,38 +8,38 @@ export default function Header() {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Package2 className="h-6 w-6" />
           <span className="sr-only">Acme Inc</span>
         </Link>
         <Link
-          href="#"
+          href="/"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           Home
         </Link>
         <Link
-          href="#"
+          href="/about"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           About
         </Link>
         <Link
-          href="#"
+          href="/blog"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           Blog
         </Link>
         <Link
-          href="#"
+          href="/faq"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           FAQ
         </Link>
         <Link
-          href="#"
+          href="/contact"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
           Contact
