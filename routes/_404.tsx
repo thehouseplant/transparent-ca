9bb0c585ce056ab143219096b1eadae30cc84397
Sean Collins <sean.collins@outlook.com>
2024-10-22 16:50:28 -0700

    Add basic Tailwind 404 template

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -4,23 +4,12 @@ export default function Error404() {
   return (
     <>
       <Head>
-        <title>404 - Page not found</title>
+        <title>404 | Page Not Found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
-        </div>
+      <div class="grid h-screen place-content-center bg-white px-4">
+        <h1 class="uppercase tracking-widest text-gray-500">
+          404 | Page Not Found
+        </h1>
       </div>
     </>
   );
